0e4800931c40e6070a1e514f6ccda28219f15cfa
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2020-06-21 14:24:11 +0700

    refactor dependency, construct with option func

--- pkg/codebase/factory/base/dependency.go
+++ pkg/codebase/factory/base/dependency.go
@@ -1,23 +1,56 @@
 package base
 
 import (
-	"agungdwiprasetyo.com/backend-microservices/config"
+	"crypto/rsa"
+
 	"agungdwiprasetyo.com/backend-microservices/pkg/middleware"
 	"agungdwiprasetyo.com/backend-microservices/pkg/publisher"
 )
 
 // Dependency base
-type Dependency struct {
-	Config     *config.Config
-	Middleware middleware.Middleware
-	Publisher  publisher.Publisher
+type Dependency interface {
+	GetMiddleware() middleware.Middleware
+	GetPublisher() publisher.Publisher
+}
+
+// Option func type
+type Option func(*deps)
+type deps struct {
+	mw         middleware.Middleware
+	pub        publisher.Publisher
+	privateKey *rsa.PrivateKey
+	publicKey  *rsa.PublicKey
+}
+
+// SetMiddleware option func
+func SetMiddleware(mw middleware.Middleware) Option {
+	return func(d *deps) {
+		d.mw = mw
+	}
+}
+
+// SetPublisher option func
+func SetPublisher(pub publisher.Publisher) Option {
+	return func(d *deps) {
+		d.pub = pub
+	}
 }
 
 // InitDependency constructor
-func InitDependency(cfg *config.Config) *Dependency {
-	return &Dependency{
-		Config:     cfg,
-		Middleware: middleware.NewMiddleware(cfg),
-		Publisher:  publisher.NewKafkaPublisher(cfg.KafkaConfig),
+func InitDependency(opts ...Option) Dependency {
+	opt := new(deps)
+
+	for _, o := range opts {
+		o(opt)
 	}
+
+	return opt
+}
+
+func (d *deps) GetMiddleware() middleware.Middleware {
+	return d.mw
+}
+
+func (d *deps) GetPublisher() publisher.Publisher {
+	return d.pub
 }
