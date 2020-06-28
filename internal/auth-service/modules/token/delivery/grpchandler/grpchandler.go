69b751c1322465bf7b3ab1b3b879fed4b439239a
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2020-06-28 23:38:49 +0700

    [auth-service]: add token validation via grpc

--- internal/auth-service/modules/token/delivery/grpchandler/grpchandler.go
+++ internal/auth-service/modules/token/delivery/grpchandler/grpchandler.go
@@ -25,10 +25,23 @@ func (h *GRPCHandler) Register(server *grpc.Server) {
 	proto.RegisterTokenHandlerServer(server, h)
 }
 
-// FindAll rpc
+// Hello rpc
 func (h *GRPCHandler) Hello(ctx context.Context, req *proto.Request) (*proto.Response, error) {
 	return &proto.Response{
 		Message: req.Message + "; Hello, from service: auth-service, module: token",
 	}, nil
 }
 
+// ValidateToken rpc
+func (h *GRPCHandler) ValidateToken(ctx context.Context, req *proto.PayloadValidate) (*proto.ResponseValidation, error) {
+	return &proto.ResponseValidation{
+		Success: true,
+		Claim: &proto.ResponseValidation_ClaimData{
+			Audience: "admin",
+			Subject:  "user",
+			User: &proto.ResponseValidation_ClaimData_UserData{
+				ID: "001",
+			},
+		},
+	}, nil
+}
