32b2dc8a2e40841c698456ecf431918c8771a8a6
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2021-11-02 13:11:16 +0700

    cli: refactor, separate cli parser & constant

--- cmd/candi/types.go
+++ cmd/candi/types.go
@@ -9,31 +9,6 @@ import (
 	"text/template"
 )
 
-const (
-	ps1                      = "\x1b[32;1m>>> \x1b[0m"
-	redFormat                = "\x1b[31;1m%s \x1b[0m\n"
-	initService              = "initservice"
-	addModule                = "addmodule"
-	initMonorepo             = "initMonorepo"
-	initMonorepoService      = "initMonorepoService"
-	addModuleMonorepoService = "addModuleMonorepoService"
-	runServiceMonorepo       = "runServiceMonorepo"
-	addHandler               = "addHandler"
-
-	restHandler             = "restHandler"
-	grpcHandler             = "grpcHandler"
-	graphqlHandler          = "graphqlHandler"
-	kafkaHandler            = "kafkaHandler"
-	schedulerHandler        = "schedulerHandler"
-	redissubsHandler        = "redissubsHandler"
-	taskqueueHandler        = "taskqueueHandler"
-	postgresListenerHandler = "postgresListenerHandler"
-	rabbitmqHandler         = "rabbitmqHandler"
-	redisDeps               = "redisDeps"
-	sqldbDeps               = "sqldbDeps"
-	mongodbDeps             = "mongodbDeps"
-)
-
 var (
 	scopeMap = map[string]string{
 		"1": initService, "2": addModule, "3": addHandler, "4": initMonorepo, "5": runServiceMonorepo,
