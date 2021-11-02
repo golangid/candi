89eaf8894ca8d56f57a258cd64e7b190a351044b
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2021-11-02 13:11:16 +0700

    cli: refactor, separate cli parser & constant

---
+++ cmd/candi/constant.go
@@ -0,0 +1,26 @@
+package main
+
+const (
+	ps1                      = "\x1b[32;1m>>> \x1b[0m"
+	redFormat                = "\x1b[31;1m%s \x1b[0m\n"
+	initService              = "initservice"
+	addModule                = "addmodule"
+	initMonorepo             = "initMonorepo"
+	initMonorepoService      = "initMonorepoService"
+	addModuleMonorepoService = "addModuleMonorepoService"
+	runServiceMonorepo       = "runServiceMonorepo"
+	addHandler               = "addHandler"
+
+	restHandler             = "restHandler"
+	grpcHandler             = "grpcHandler"
+	graphqlHandler          = "graphqlHandler"
+	kafkaHandler            = "kafkaHandler"
+	schedulerHandler        = "schedulerHandler"
+	redissubsHandler        = "redissubsHandler"
+	taskqueueHandler        = "taskqueueHandler"
+	postgresListenerHandler = "postgresListenerHandler"
+	rabbitmqHandler         = "rabbitmqHandler"
+	redisDeps               = "redisDeps"
+	sqldbDeps               = "sqldbDeps"
+	mongodbDeps             = "mongodbDeps"
+)
