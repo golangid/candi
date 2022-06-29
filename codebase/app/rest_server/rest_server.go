481011ec96790226f6547d56fd6d289eb6b1d140
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2022-06-29 22:01:18 +0700

    rest server: update shared listener matcher

--- codebase/app/rest_server/rest_server.go
+++ codebase/app/rest_server/rest_server.go
@@ -43,7 +43,7 @@ func NewServer(service factory.ServiceFactory, opts ...OptionFunc) factory.AppSe
 	}
 
 	if server.opt.sharedListener != nil {
-		server.listener = server.opt.sharedListener.Match(cmux.Any())
+		server.listener = server.opt.sharedListener.Match(cmux.HTTP1Fast("PATCH"))
 	}
 
 	server.serverEngine.HTTPErrorHandler = server.opt.errorHandler
