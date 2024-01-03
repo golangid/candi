103fb7bdf2f250fb83d018b6850bbbe079247548
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2024-01-03 18:00:24 +0700

    redis db: add default pool config

--- config/database/redis.go
+++ config/database/redis.go
@@ -2,12 +2,30 @@ package database
 
 import (
 	"context"
+	"log"
+	"time"
+
 	"github.com/golangid/candi/cache"
 	"github.com/golangid/candi/codebase/interfaces"
 	"github.com/golangid/candi/config/env"
 	"github.com/golangid/candi/logger"
 	"github.com/gomodule/redigo/redis"
 )
+
+type RedisPoolOption func(pool *redis.Pool)
+
+func RedisPoolOptionSetMaxIdle(count int) RedisPoolOption {
+	return func(pool *redis.Pool) { pool.MaxIdle = count }
+}
+func RedisPoolOptionSetMaxActive(count int) RedisPoolOption {
+	return func(pool *redis.Pool) { pool.MaxActive = count }
+}
+func RedisPoolOptionSetIdleTimeout(dur time.Duration) RedisPoolOption {
+	return func(pool *redis.Pool) { pool.IdleTimeout = dur }
+}
+func RedisPoolOptionSetMaxConnLifetime(dur time.Duration) RedisPoolOption {
+	return func(pool *redis.Pool) { pool.MaxConnLifetime = dur }
+}
 
 type redisInstance struct {
 	read, write *redis.Pool
@@ -50,31 +68,40 @@ func (m *redisInstance) Disconnect(ctx context.Context) (err error) {
 
 // InitRedis connection from environment:
 // REDIS_READ_DSN, REDIS_WRITE_DSN
-func InitRedis() interfaces.RedisPool {
+func InitRedis(opts ...RedisPoolOption) interfaces.RedisPool {
 	deferFunc := logger.LogWithDefer("Load Redis connection...")
 	defer deferFunc()
 
 	inst := &redisInstance{
-		read:  ConnectRedis(env.BaseEnv().DbRedisReadDSN),
-		write: ConnectRedis(env.BaseEnv().DbRedisWriteDSN),
+		read:  ConnectRedis(env.BaseEnv().DbRedisReadDSN, opts...),
+		write: ConnectRedis(env.BaseEnv().DbRedisWriteDSN, opts...),
 	}
 	inst.cache = cache.NewRedisCache(inst.read, inst.write)
 	return inst
 }
 
 // ConnectRedis connect to redis with dsn
-func ConnectRedis(dsn string) *redis.Pool {
+func ConnectRedis(dsn string, opts ...RedisPoolOption) *redis.Pool {
 	pool := &redis.Pool{
 		Dial: func() (redis.Conn, error) {
 			return redis.DialURL(dsn)
 		},
+
+		// default pool config
+		MaxIdle:         80,
+		MaxActive:       500,
+		IdleTimeout:     20 * time.Minute,
+		MaxConnLifetime: 1 * time.Hour,
+	}
+	for _, opt := range opts {
+		opt(pool)
 	}
 
 	ping := pool.Get()
 	defer ping.Close()
 	_, err := ping.Do("PING")
 	if err != nil {
-		panic("redis ping: " + err.Error())
+		log.Panicf("redis ping: %s", err.Error())
 	}
 
 	return pool
